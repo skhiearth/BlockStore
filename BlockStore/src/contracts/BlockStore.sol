db1af16f069a46e6142d9d9ecc323aafe5aa4a4d
skhiearth <skhiearth@gmail.com>
2021-05-15 19:34:15 +0530

    smartcontract
    Co-Authored-By: Simran Gogia <70960753+simmsss@users.noreply.github.com>

--- BlockStore/src/contracts/BlockStore.sol
+++ BlockStore/src/contracts/BlockStore.sol
@@ -3,19 +3,124 @@
 pragma solidity >=0.5.0;
 
 contract BlockStore {
-
-    string name;
-
-    constructor() public {
-        name = "BlockStore";
+    
+    // Track total number of products, orders and sellers
+    uint public productCount = 0;
+    uint public orderCount = 0;
+    uint public sellerCount = 0;
+    
+    // Mappings to keep track of structs
+    mapping(address => Seller) public Sellers;
+    mapping(address => Buyer) Buyers;
+    mapping(uint => Product) Products;
+    
+    // List of all product
+    Product[] public ProductList;
+    
+    // Get orders for a user
+    mapping(address => Order[]) UserOrders;
+    mapping(address => PlacedOrders[]) SellerOrders;
+    
+    
+    // Seller
+    struct Seller {
+        string name;
+        address publicAddress;
+        bool created;
     }
-
-    function getName() public view returns (string memory) {
-        return name;
+    
+    // Buyer
+    struct Buyer {
+        string name;
+        string shippingAddress;
     }
-
-    function setName(string calldata newName) external {
-        name = newName;
+    
+    // Product
+    struct Product {
+        uint productId;
+        string name;
+        string description;
+        uint price;
+        address payable seller;
+    }
+    
+    // Order
+    struct Order {
+        uint productId;
+        uint orderId;
+        string status;
+    }
+    
+    // PlacedOrder
+    struct PlacedOrders {
+        uint productId;
+        uint orderId;
+        string status;
+        address buyer;
+    }
+    
+    
+    // Create a new seller
+    function createSeller(string memory _name) public {
+        Sellers[msg.sender].name = _name; // Create new instance - specify name
+        Sellers[msg.sender].publicAddress = msg.sender; // Specify public address
+        Sellers[msg.sender].created = true; // Validate creation
+        sellerCount = sellerCount + 1; // Increment for subsequent creation
+    }
+    
+    // Create a new buyer
+    function createBuyer(string memory _name, string memory _shippingAddress) public {
+        Buyers[msg.sender].name = _name; // Create new instance - specify name
+        Buyers[msg.sender].shippingAddress = _shippingAddress; // Specify shipping address
+    }
+    
+    // Create a new product
+    function createProduct(string memory _name, string memory _description, uint _price) public {
+        Product memory product = Product(productCount, _name, _description, _price, msg.sender); // Create new instance of struct
+        Products[productCount].productId = productCount;
+        Products[productCount].name = _name;
+        Products[productCount].description = _description;
+        Products[productCount].price = _price;
+        Products[productCount].seller = msg.sender;
+        ProductList.push(product);
+        
+        productCount = productCount + 1; // Increment product count for subsequent products
+    }
+    
+    // Create a new order
+    function createOrder(uint _id) public payable {
+        require(msg.value == Products[_id].price, "Incorrect value of funds");
+        
+        Order memory order = Order(_id, orderCount, "Order Placed"); // Create new instance of struct
+        UserOrders[msg.sender].push(order); // Add order to list of users' orders
+        
+        PlacedOrders memory placedOrder = PlacedOrders(_id, orderCount, "Order Placed", msg.sender); // Create new instance of struct
+        SellerOrders[Products[_id].seller].push(placedOrder); // Add placed order to list of sellers' orrders
+        
+        Products[_id].seller.transfer(msg.value); // Transfer funds to seller
+        
+        orderCount = orderCount + 1; // Increment order count for subsequent orders
+    }
+    
+    // Update the status of order
+    function updateOrderStatus(uint _index, string memory _newStatus) public {
+        PlacedOrders memory sellerOrder = SellerOrders[msg.sender][_index];
+        sellerOrder.status = _newStatus;
+        SellerOrders[msg.sender][_index] = sellerOrder; // Update seller's order
+        
+        Order memory buyerOrder = UserOrders[sellerOrder.buyer][_index];
+        buyerOrder.status = _newStatus;
+        UserOrders[sellerOrder.buyer][_index] = buyerOrder; // Update buyer's order
+    }
+    
+    // Return all orders placed by a buyer
+    function getBuyerOrders(uint _index) public view returns (uint, uint, string memory) {
+        return(UserOrders[msg.sender][_index].productId, UserOrders[msg.sender][_index].orderId, UserOrders[msg.sender][_index].status);
+    }
+    
+    // Return all orders recieved by a seller
+    function getSellerOrders(uint _index) public view returns (uint, uint, string memory, address) {
+        return(SellerOrders[msg.sender][_index].productId, SellerOrders[msg.sender][_index].orderId, SellerOrders[msg.sender][_index].status, SellerOrders[msg.sender][_index].buyer);
     }
     
 }
