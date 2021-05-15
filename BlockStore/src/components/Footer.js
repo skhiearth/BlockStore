9c0cb8667051ecf85c80c2c56e12b8f55deaf4b7
skhiearth <skhiearth@gmail.com>
2021-05-15 18:40:50 +0530

    footer and navbar

---
+++ BlockStore/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from './App.module.css';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <footer class="py-1 fixed-bottom" style={{height:80, position: "relative", bottom: 0}}>
+        <div class="container" style={{padding:20}}>
+          <p class="m-0 text-center text-dark" className={styles.footerHeader}>
+            Made with <span role="img" aria-label="love">🙌  </span>  by The Misfits
+          </p>
+          <p class="m-0 text-center text-dark" className={styles.footerCredits}>
+            Simran Gogia  |  Utkarsh Sharma
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
